b030cf2783b6b0f236d7962f1621e9f6321f1031
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-04-10 16:22:26 -0700

    Replaced React.render() with ReactDOM.render()

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 "use strict";
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 // Create a new component. This component should produce some HTML
 
-const App = function(){
-    return <div>Hi!</div>;
-};
+const App = () => (<div>Hi!</div>);
 
 // Take this component's generated HTML and put it on the page (in the DOM)
 
-React.render(App);
+ReactDOM.render(App);
