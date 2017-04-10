9c39f110287a46101b7be401bcd74331b76a4938
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-04-10 16:52:03 -0700

    App now attaches to the DOM

--- src/index.js
+++ src/index.js
@@ -10,4 +10,4 @@ const App = () => (
 
 // Take this component's generated HTML and put it on the page (in the DOM)
 
-ReactDOM.render(<App />);
+ReactDOM.render(<App />, document.querySelector('.container'));
