a2bcee2f54a941a9c9adb222f0ae5c743c63b1d5
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-05-01 18:24:41 -0700

    added Youtube API test search

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 "use strict";
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBgsow1ilciXuIhNZq33uwEs12VQRI7yPw';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(response){
+    console.log(response);
+});
 
 // Create a new component. This component should produce some HTML
 
