5b56873cf883b709149e31fc3b7c716c8242e382
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-05-01 19:55:20 -0700

    set up html structure for VideoListItem components

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,21 @@
 "use strict";
 import React from 'react';
 
-const VideoListItem = props => (
-    <li>{props.video.snippet.title}</li>
-);
+const VideoListItem = ({video: {snippet: {title, thumbnails}}}) => {
+    const imageUrl = thumbnails.default.url;
+
+    return (
+        <li className="list-group-item">
+            <div className="video-list media">
+                <div className="media-left">
+                    <img className="media-object" src={imageUrl} />
+                </div>
+                <div className="media-body">
+                    <div className="media-heading">{title}</div>
+                </div>
+            </div>
+        </li>
+    );
+};
 
 export default VideoListItem;
