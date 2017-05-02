9ae4ae389b275896a6144f80cebc1f8ec769d57a
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-05-01 19:02:37 -0700

    added VideoList component
    added VideoList component
    passed list of videos from App to VideoList

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyBgsow1ilciXuIhNZq33uwEs12VQRI7yPw';
 
 class App extends Component {
@@ -20,6 +21,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar/>
+                <VideoList videos={this.state.videos}/>
             </div>
         );
     }
