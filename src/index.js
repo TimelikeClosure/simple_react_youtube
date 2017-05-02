ff53af9670daa76916f97e7b92a365561142e465
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-05-01 18:43:11 -0700

    converted App to class component
    converted App to class-based component
    initialized state to empty video list
    moved Youtube sample search into App constructor
    changed Youtube sample search to update state instead of logging to console

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 "use strict";
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyBgsow1ilciXuIhNZq33uwEs12VQRI7yPw';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(response){
-    console.log(response);
-});
+class App extends Component {
+    constructor(props){
+        super(props);
 
-// Create a new component. This component should produce some HTML
+        this.state = {videos: []};
 
-const App = () => (
-    <div>
-        <SearchBar/>
-    </div>
-);
+        YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+            this.setState({videos});
+        });
+    }
 
-// Take this component's generated HTML and put it on the page (in the DOM)
+    render(){
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
