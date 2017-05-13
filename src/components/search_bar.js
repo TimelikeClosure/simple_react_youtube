560f13593e8985d29854966a3fe5a2a15dc75ce6
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-05-13 14:50:25 -0700

    added dynamic searches

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,7 +5,12 @@ class SearchBar extends Component {
     constructor(props){
         super(props);
 
-        this.state = {term: 'Starting Value!!!'};
+        this.state = {term: ''};
+    }
+
+    onInputChange(term){
+        this.setState({term});
+        this.props.onSearchTermChange(term);
     }
 
     render() {
@@ -13,7 +18,7 @@ class SearchBar extends Component {
             <div className="search-bar">
                 <input
                     value={this.state.term}
-                    onChange={event => this.setState({term: event.target.value})}
+                    onChange={event => this.onInputChange(event.target.value)}
                 />
             </div>
         );
